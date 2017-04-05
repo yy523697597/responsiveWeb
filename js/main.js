3946e8566f45791414c08230d86a315b70602b5f
yu yi <523697597@qq.com>
2017-04-05 21:35:43 +0800

    V1.0 第一次整体上传
    V1.0 第一次整体上传

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/11/16 0016.
+ */
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
+    });
+});
